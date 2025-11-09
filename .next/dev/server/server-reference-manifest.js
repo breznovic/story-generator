a25e43ae31a74440ea453f0ae2fcface0d4cacf0
breznovic <slymarkel@gmail.com>
2025-11-09 15:12:43 +0300

    fix: add .next to gitignore and remove from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"39tNvyLF7UeqPF0dT1j6I+L8WhnyPdzHPhIReOTrM+Y=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_11117d05._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8544f796._.css",
-  "static/chunks/node_modules__pnpm_d1888275._.js",
-  "static/chunks/_b8f8c3eb._.js"
-],
-    source: "dynamic"
-});
